7edea9c6afcf64bd80cb5821f8a760408270dd18
Gabriel Revells <grevells@nutshell.com>
2017-11-17 13:24:47 -0500

    Add testing file for NAI helpers

---
+++ Example/Tests/NetworkActivityHelperTests.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkActivityHelperTests.swift
+//  RESTAPILibrary_Tests
+//
+//  Created by Gabriel Revells on 11/17/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import XCTest
+@testable import RESTAPILibrary
+
+class NetworkActivityHelperTests: XCTestCase {
+    
+}
